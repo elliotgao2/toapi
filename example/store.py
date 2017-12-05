d995350d9c32e6c054fa85efcb564c5da515d4ea
wuqiangroy <wuqiangroy@live.com>
2017-12-05 22:37:06 +0800

    python2 compatible

--- example/store.py
+++ example/store.py
@@ -3,8 +3,6 @@ from toapi.storage import Store
 store = Store()
 
 url = "https://www.google.com..."
-html = """
-<p> Hello, World!</p>
-"""
-# store.save(url, html)
+html = "<p> Hello, World!</p>"
+store.save(url, html)
 print(store.get(url))
--- toapi/storage.py
+++ toapi/storage.py
@@ -11,23 +11,22 @@ import hashlib
 
 class Store:
 
-    def __init__(self, path=None):
+    path = os.getcwd()
+
+    def __init__(self, path=path):
         try:
             os.listdir(path)
-        except FileNotFoundError as e:
+        except Exception as e:
             raise TypeError("Please input correct path")
 
-        if path:
-            if not os.path.exists(path+".html"):
-                os.mkdir(path+".html")
-            if path and path.endswith("/"):
-                self.path = path + ".html/"
-            elif path and not path.endswith("/"):
-                self.path = path + "/.html/"
+        if path.endswith("/"):
+            if not os.path.exists(path + ".html"):
+                os.mkdir(path + ".html")
+            self.path = path + ".html/"
         else:
-            if not os.path.exists(".html"):
-                os.mkdir(".html")
-            self.path = ".html/"
+            if not os.path.exists(path + "/.html"):
+                os.mkdir(path + "/.html")
+            self.path = path + "/.html/"
 
     def save(self, url, html):
         file_name = hashlib.md5(url.encode()).hexdigest()
