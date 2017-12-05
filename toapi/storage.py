d3f7450f8943f50f0b7e9c449daf16f2e2689308
wuqiangroy <wuqiangroy@live.com>
2017-12-05 21:30:57 +0800

    storage

--- toapi/storage.py
+++ toapi/storage.py
@@ -11,14 +11,35 @@ import hashlib
 
 class Store:
 
-    def __init__(self):
-        if not os.path.exists(".html"):
-            os.mkdir(".html")
+    def __init__(self, path=None):
+        try:
+            os.listdir(path)
+        except FileNotFoundError as e:
+            raise TypeError("Please input correct path")
+
+        if path:
+            if not os.path.exists(path+".html"):
+                os.mkdir(path+".html")
+            if path and path.endswith("/"):
+                self.path = path + ".html/"
+            elif path and not path.endswith("/"):
+                self.path = path + "/.html/"
+        else:
+            if not os.path.exists(".html"):
+                os.mkdir(".html")
+            self.path = ".html/"
 
     def save(self, url, html):
-        pass
+        file_name = hashlib.md5(url.encode()).hexdigest()
+        with open(self.path+file_name, "wb") as f:
+            f.write(html.encode())
+        return True
 
     def get(self, url):
-        pass
-
-
+        file_name = hashlib.md5(url.encode()).hexdigest()
+        files = os.listdir(self.path)
+        if file_name not in files:
+            return {"status": False, "data": None}
+        with open(self.path+file_name, "rb") as f:
+            data = f.readlines()
+        return {"status": True, "data": data}
