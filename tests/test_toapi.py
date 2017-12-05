c7fffca6c1edfb767eba494ae5a450a0b47aaa7a
Jiuli Gao <gaojiuli@gmail.com>
2017-12-05 23:32:17 +0800

    Update test_toapi.py

--- tests/test_toapi.py
+++ tests/test_toapi.py
@@ -1,21 +1,3 @@
-def test_api():
-    from toapi import XPath, Item, Api
-
-    api = Api('https://news.ycombinator.com/')
-
-    class Post(Item):
-        url = XPath('//a[@class="storylink"][1]/@href')
-        title = XPath('//a[@class="storylink"][1]/text()')
-
-        class Meta:
-            source = XPath('//tr[@class="athing"]')
-            route = '/'
-
-    api.register(Post)
-
-    print(api.parse('/'))
-
-
 def test_api_with_ajax():
     from toapi import XPath, Item, Api
 
