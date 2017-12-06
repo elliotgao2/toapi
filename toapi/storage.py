2bdf1315c1af390b28cc3b1a04dafaec69edaccc
wuqiangroy <wuqiangroy@live.com>
2017-12-06 22:29:27 +0800

    db store

--- toapi/storage.py
+++ toapi/storage.py
@@ -7,9 +7,10 @@ store the html content and url
 
 import os
 import hashlib
+import records
 
 
-class Store:
+class DiskStore:
 
     path = os.getcwd()
 
@@ -36,9 +37,43 @@ class Store:
 
     def get(self, url):
         file_name = hashlib.md5(url.encode()).hexdigest()
-        files = os.listdir(self.path)
-        if file_name not in files:
+        try:
+            with open(self.path+file_name, "rb") as f:
+                data = f.readlines()
+            return {"status": True, "data": data}
+        except Exception as e:
             return {"status": False, "data": None}
-        with open(self.path+file_name, "rb") as f:
-            data = f.readlines()
-        return {"status": True, "data": data}
+
+
+class DBStore:
+
+    def __init__(self, storage:dict):
+        """
+        about storage, storage is a dict including keys DB_URL and NAME
+        support database: mysql, postgresql, sqlite, oracle e.t.
+        Mysql:
+        storage = {
+            "DB_URL": "mysql://name:password@host/dbname",
+        }
+        and so on
+        """
+
+        db_url = storage.get("DB_URL")
+        self.db = records.Database(db_url)
+        self.db.query("""CREATE TABLE IF NOT EXISTS `ToApi`(`url` VARCHAR(100),
+                         `html` MEDIUMTEXT NOT NULL,PRIMARY KEY ( `url` ))ENGINE=InnoDB DEFAULT CHARSET=utf8;""")
+
+    def save(self, url, html):
+        file_name = hashlib.md5(url.encode()).hexdigest()
+        self.db.query("INSERT INTO ToApi (url, html) VALUES ('{}', '{}') ".format(file_name, html))
+        return True
+
+    def get(self, url):
+        file_name = hashlib.md5(url.encode()).hexdigest()
+
+        row = self.db.query("SELECT html FROM ToApi where url='{}'".format(file_name)).first()
+        try:
+            data = dict(row).get("html")
+        except TypeError as e:
+            return None
+        return data
