bb6c3af9a983e3131af85319c733f94437adb1b7
howie.hu <howie6879@gmail.com>
2017-12-07 21:00:28 +0800

    Add: use cached in parse (#33)

--- toapi/cache/__init__.py
+++ toapi/cache/__init__.py
@@ -1,5 +1,6 @@
-from .redis_cache import RedisCache
+from .cache_setting import CacheSetting
+from .decorator import cached
 from .memcached_cache import MemcachedCache
 from .memory_cache import MemoryCache
+from .redis_cache import RedisCache
 from .serializer import JsonSerializer, PickleSerializer
-from .decorator import cached
