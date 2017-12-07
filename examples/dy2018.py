62242731fb7416bb5987faa3aec90c8bbc513951
p <97676212@qq.com>
2017-12-08 00:35:04 +0800

    Multi source site support

--- examples/dy2018.py
+++ examples/dy2018.py
@@ -1,9 +1,9 @@
 from toapi import XPath, Item, Api
 
-api = Api('http://www.dy2018.com/')
+api = Api('http://www.dy2018.com')
 
 
-class Movies(Item):
+class Movie(Item):
     url = XPath('//b//a[@class="ulink"]/@href')
     title = XPath('//b//a[@class="ulink"]/text()')
 
@@ -12,6 +12,30 @@ class Movies(Item):
         route = '/html/gndy/dyzz/index_\d+.html'
 
 
-api.register(Movies)
+class Post(Item):
+    __base_url__ = 'https://news.ycombinator.com/'
+    url = XPath('//a[@class="storylink"]/@href')
+    title = XPath('//a[@class="storylink"]/text()')
+
+    class Meta:
+        source = XPath('//tr[@class="athing"]')
+        route = '/news\?p=\d+'
+
+
+class Page(Item):
+    __base_url__ = 'https://news.ycombinator.com/'
+    next_page = XPath('//a[@class="morelink"]/@href')
+
+    class Meta:
+        source = None
+        route = '/news\?p=\d+'
+
+    def clean_next_page(self, next_page):
+        return "http://127.0.0.1:5000/" + next_page
+
+
+api.register(Movie)
+api.register(Post)
+api.register(Page)
 
 api.serve()
