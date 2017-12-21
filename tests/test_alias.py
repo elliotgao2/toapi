d269a4b78e7a6278a01eb081e488983175b7ccef
howie.hu <howie6879@gmail.com>
2017-12-21 19:02:37 +0800

    Add: alias test (#84)
    * :memo: Update cache.md
    
    * :memo: Update examples
    
    * :bug: Fixed:https://github.com/gaojiuli/toapi/issues/79
    
    * :bug: Fixed:https://github.com/gaojiuli/toapi/issues/80
    
    * Add: alias test

--- tests/test_alias.py
+++ tests/test_alias.py
@@ -3,6 +3,18 @@ from toapi import Api
 
 def test_alias():
     api = Api()
+    assert api.convert_route_to_alias('/movies/22/?output=utf-8',
+                                      '/movies/\d{1,2}/?output=:page',
+                                      '/movies/test/:page.html') == '/movies/test/utf-8.html'
+
+    assert api.convert_route_to_alias('/movies/22/?你好=你好',
+                                      '/movies/\d{1,2}/?你好=:page',
+                                      '/movies/test/:page.html') == '/movies/test/你好.html'
+
+    assert api.convert_route_to_alias('/movies/22/?page=2',
+                                      '/movies/\d{1,2}/?page=:page',
+                                      '/movies/test/index_:page.html') == '/movies/test/index_2.html'
+
     assert api.convert_route_to_alias('/movies/?page=2',
                                       '/movies/?page=:page',
                                       '/html/gndy/dyzz/index_:page.html') == '/html/gndy/dyzz/index_2.html'
@@ -15,3 +27,5 @@ def test_alias():
     assert api.convert_route_to_alias('/movies/you/?page=2&a=1',
                                       '/:path',
                                       '/:path') == '/movies/you/?page=2&a=1'
+
+test_alias()
