b15c9a0c88c43e88a886cd78b6abaa16812e7f64
gaojiuli <gaojiuli@gmail.com>
2018-03-04 20:10:50 +0800

    only support 3.6+

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
     install_requires=[
